092e64ca55058aedc0be8278540b598205e82cc1
Dominik Czerwiński <dominikczerwinski@gmail.com>
2023-02-21 12:42:06 +0000

    Merged PR 9: Azure devops

--- src/ChoirManagement.Accounting.WebService/Program.cs
+++ src/ChoirManagement.Accounting.WebService/Program.cs
@@ -1,6 +1,7 @@
+var instanceId = Guid.NewGuid();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"Hello World from instance {instanceId}");
 
 app.Run();
